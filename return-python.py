739d55df8dc00c7ceb69dad2d0fbd5008c4e2f2d
Vitalik <humenchukvitalik@gmail.com>
2026-05-26 03:57:21 +0300

    Day 2: Prime numbers ✅ Streak: 2

---
+++ return-python.py
@@ -0,0 +1,39 @@
+# n = int(input("Введіть число для функції Фібоначі:"))
+
+# def fibonacci(n):
+#     if n == 0:
+#         return []
+    
+#     if n <= 1:
+#         return[0]
+    
+#     fib = [0,1]
+
+#     for i in range(2,n):
+#         new_number = fib[i-1] + fib[i-2]
+#         fib.append(new_number)
+#     return fib
+
+# print(fibonacci(n))
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    
+    return True
+
+# Тести
+print(is_prime(2))   # True
+print(is_prime(7))   # True
+print(is_prime(10))  # False
+print(is_prime(13))  # True
+print(is_prime(9))   # False
